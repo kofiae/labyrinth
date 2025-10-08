dd9c892d7dbe830e3d183fbb9d26e2d8cdd34e51
Bussy <olubusolaogoe@gmail.com>
2025-10-08 14:37:11 +0200

    Atelier 1 - Entités du projet

---
+++ Labyrinth/Labyrinth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    public class Labyrinth
+    {
+        private Tile[,] grid;
+        private int width;
+        private int height;
+
+        public int Width
+        {
+            get { return width; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+        }
+            
+
+        public Labyrinth(string chemin)
+        {
+            var lignes = chemin.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            this.height = lignes.Length;
+            this.width = lignes[0].Length;
+
+            grid = new Tile[height, width];
+        }
+    }
+}
---
+++ Labyrinth/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    internal class Room : Tile
+    {
+        private Collectable? item;
+        public override bool IsTraversable => true;
+
+        public Collectable Item { get => Item; set => Item = value; }
+
+        public override void Pass()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
