d1c2bd4e7e7fe4cf22f1eccc146a314256d8db11
elia-leprevost <elia.leprevost@efrei.net>
2025-11-05 09:37:18 +0100

    Atelier 3 - Partie 3

---
+++ Labyrinth/CrawlingEventsArgs.cs
@@ -0,0 +1,22 @@
+using System;
+using Labyrinth.Crawl;
+
+namespace Labyrinth
+{
+
+    public class CrawlingEventArgs : EventArgs
+    {
+        public CrawlingEventArgs(int x, int y, Direction direction)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public Direction Direction { get; }
+    }
+}
--- Labyrinth/Explorer.cs
+++ Labyrinth/Explorer.cs
@@ -8,21 +8,34 @@ namespace Labyrinth
     {
         private readonly ICrawler _crawler;
 
+    
         public Random Random { get; set; } = new Random();
+
+    
+        public event EventHandler<CrawlingEventArgs>? PositionChanged;
+
+    
+        public event EventHandler<CrawlingEventArgs>? DirectionChanged;
 
         public Explorer(ICrawler crawler)
         {
             _crawler = crawler ?? throw new ArgumentNullException(nameof(crawler));
         }
+
+    
         public bool GetOut(int n)
         {
             if (n <= 0) return false;
+
+            EmitDirectionChanged();
+            EmitPositionChanged();
 
             for (int i = 0; i < n; i++)
             {
                 if (_crawler.FacingTile is Outside)
                 {
                     _crawler.Walk();
+                    EmitPositionChanged();
                     return true;
                 }
 
@@ -31,38 +44,48 @@ namespace Labyrinth
                 switch (action)
                 {
                     case 0:
-                    
+                        
                         if (!(_crawler.FacingTile is Wall))
                         {
                             try
                             {
                                 _crawler.Walk();
+                                EmitPositionChanged();
                             }
                             catch
                             {
-                            
+                                
                                 _crawler.Direction.TurnRight();
+                                EmitDirectionChanged();
                             }
                         }
                         else
                         {
-                        
+                            
                             _crawler.Direction.TurnRight();
+                            EmitDirectionChanged();
                         }
                         break;
 
                     case 1:
                         _crawler.Direction.TurnLeft();
+                        EmitDirectionChanged();
                         break;
 
                     case 2:
                         _crawler.Direction.TurnRight();
+                        EmitDirectionChanged();
                         break;
                 }
-
             }
 
             return false;
         }
+
+        private void EmitPositionChanged() =>
+            PositionChanged?.Invoke(this, new CrawlingEventArgs(_crawler.X, _crawler.Y, _crawler.Direction));
+
+        private void EmitDirectionChanged() =>
+            DirectionChanged?.Invoke(this, new CrawlingEventArgs(_crawler.X, _crawler.Y, _crawler.Direction));
     }
 }
